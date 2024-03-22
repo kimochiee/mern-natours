aa3a0114c3fc28f0c2e95564f9d23b644f082578
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-03-22 19:53:31 +0700

    getTourStats and getMonthlyPlan

--- api/src/controllers/tour.controller.js
+++ api/src/controllers/tour.controller.js
@@ -74,3 +74,75 @@ export const deleteTour = async (req, res) => {
     res.status(400).json({ status: 'fail', message: error.message })
   }
 }
+
+export const getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } }
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' }
+        }
+      },
+      {
+        $sort: { avgPrice: 1 }
+      }
+    ])
+
+    res.status(200).json({ status: 'success', data: { stats } })
+  } catch (error) {
+    console.log(error)
+
+    res.status(400).json({ status: 'fail', message: error.message })
+  }
+}
+
+export const getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates'
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: `${year}-01-01`,
+            $lte: `${year}-12-31`
+          }
+        }
+      },
+      {
+        $group: {
+          _id: { $month: { $toDate: '$startDates' } },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' }
+        }
+      },
+      {
+        $addFields: { month: '$_id' }
+      },
+      {
+        $project: { _id: 0 }
+      },
+      {
+        $sort: { numTourStarts: -1 }
+      }
+    ])
+
+    res.status(200).json({ status: 'success', data: { plan } })
+  } catch (error) {
+    console.log(error)
+
+    res.status(400).json({ status: 'fail', message: error.message })
+  }
+}
--- api/src/routes/tour.routes.js
+++ api/src/routes/tour.routes.js
@@ -7,10 +7,14 @@ import {
   getTour,
   createTour,
   updateTour,
-  deleteTour
+  deleteTour,
+  getTourStats,
+  getMonthlyPlan
 } from '~/controllers/tour.controller'
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours)
+router.route('/tour-stats').get(getTourStats)
+router.route('/monthly-plan/:year').get(getMonthlyPlan)
 
 router.route('/').get(getAllTours).post(createTour)
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
