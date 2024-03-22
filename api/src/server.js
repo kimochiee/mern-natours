09a7aa85caf2377781b74e7f46cf049f60b2a6f2
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-03-22 11:44:18 +0700

    serving static file

--- api/src/server.js
+++ api/src/server.js
@@ -7,6 +7,7 @@ import router from './routes'
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(`${__dirname}/public`))
 
 app.use('/api/v1', router)
 
