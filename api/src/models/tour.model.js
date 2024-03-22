f6a89e70e67c16e92530953c0a0a7070eac94a63
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-03-22 19:57:12 +0700

    virtual property durationWeeks

--- api/src/models/tour.model.js
+++ api/src/models/tour.model.js
@@ -1,96 +1,107 @@
 import mongoose from 'mongoose'
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty']
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price']
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a summary']
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false
+    },
+    startDates: [Date]
+    // secretTour: {
+    //   type: Boolean,
+    //   default: false
+    // },
+    // startLocation: {
+    //   // GeoJSON
+    //   type: {
+    //     type: String,
+    //     default: 'Point',
+    //     enum: ['Point']
+    //   },
+    //   coordinates: [Number],
+    //   address: String,
+    //   description: String
+    // },
+    // locations: [
+    //   {
+    //     type: {
+    //       type: String,
+    //       default: 'Point',
+    //       enum: ['Point']
+    //     },
+    //     coordinates: [Number],
+    //     address: String,
+    //     description: String,
+    //     day: Number
+    //   }
+    // ],
+    // guides: [
+    //   {
+    //     type: mongoose.Schema.ObjectId,
+    //     ref: 'User'
+    //   }
+    // ],
+    // reviews: [
+    //   {
+    //     type: mongoose.Schema.ObjectId,
+    //     ref: 'Review'
+    //   }
+    // ]
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration']
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size']
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty']
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a summary']
-  },
-  description: {
-    type: String,
-    trim: true
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image']
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false
-  },
-  startDates: [Date]
-  // secretTour: {
-  //   type: Boolean,
-  //   default: false
-  // },
-  // startLocation: {
-  //   // GeoJSON
-  //   type: {
-  //     type: String,
-  //     default: 'Point',
-  //     enum: ['Point']
-  //   },
-  //   coordinates: [Number],
-  //   address: String,
-  //   description: String
-  // },
-  // locations: [
-  //   {
-  //     type: {
-  //       type: String,
-  //       default: 'Point',
-  //       enum: ['Point']
-  //     },
-  //     coordinates: [Number],
-  //     address: String,
-  //     description: String,
-  //     day: Number
-  //   }
-  // ],
-  // guides: [
-  //   {
-  //     type: mongoose.Schema.ObjectId,
-  //     ref: 'User'
-  //   }
-  // ],
-  // reviews: [
-  //   {
-  //     type: mongoose.Schema.ObjectId,
-  //     ref: 'Review'
-  //   }
-  // ]
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+)
+
+// virtual properties
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7
 })
 
 export const Tour = mongoose.model('Tour', tourSchema)
