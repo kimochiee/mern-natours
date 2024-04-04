3c5308665998946bee433d4c0ea41e2338ab796c
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-04-04 19:48:16 +0700

    add sign up page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <ToastContainer
         toastStyle={{ fontSize: "1.4rem" }}
         position="bottom-left"
-        autoClose={1500}
+        autoClose={1000}
         pauseOnHover={false}
       />
     </Provider>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,11 +1,68 @@
-
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import { signInFailure, signInStart, signInSuccess } from "../redux/user/userSlice"
+import { notify } from "../utils/notify"
 
 function Signup() {
+  const [formData, setFormData] = useState({})
+  const { loading } = useSelector((state) => state.user)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value.trim()
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (!formData.name || !formData.email || !formData.password || !formData.passwordConfirm) {
+      return dispatch(signInFailure('Please fill all the fields'))
+    }
+
+    if (formData.password !== formData.passwordConfirm) {
+      return dispatch(signInFailure('Passwords do not match'))
+    }
+
+    try {
+      dispatch(signInStart())
+
+      const res = await fetch('http://localhost:8000/api/v1/users/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        credentials: 'include',
+        body: JSON.stringify(formData)
+      })
+
+      const data = await res.json()
+
+      if (!res.ok) {
+        notify(data.message, 'error')
+        return dispatch(signInFailure(data.message))
+      }
+
+      if (data.status === 'success') {
+        notify('Sign up successful', 'success')
+        dispatch(signInSuccess(data.data.user))
+        return navigate('/');
+      }
+    } catch (error) {
+      dispatch(signInFailure(error.message))
+      console.error(error.message)
+    }
+  }
+
   return (
     <main className="main">
       <div className="login-form">
         <h2 className="heading-secondary ma-bt-lg">Create new account</h2>
-        <form className="form form--login">
+        <form className="form form--login" onSubmit={handleSubmit}>
           <div className="form__group">
             <label className="form__label" htmlFor="email">Your name</label>
             <input
@@ -14,6 +71,7 @@ function Signup() {
               type="text"
               placeholder="John Doe"
               required="required"
+              onChange={handleChange}
             />
           </div>
           <div className="form__group">
@@ -24,6 +82,7 @@ function Signup() {
               type="email"
               placeholder="you@example.com"
               required="required"
+              onChange={handleChange}
             />
           </div>
           <div className="form__group ma-bt-md">
@@ -35,6 +94,7 @@ function Signup() {
               placeholder="••••••••"
               required="required"
               minLength="8"
+              onChange={handleChange}
             />
           </div>
           <div className="form__group ma-bt-md">
@@ -46,10 +106,15 @@ function Signup() {
               placeholder="••••••••"
               required="required"
               minLength="8"
+              onChange={handleChange}
             />
           </div>
           <div className="form__group">
-            <button className="btn btn--green">Sign up</button>
+            {loading ? (
+              <button className="btn btn--green" disabled>Loading...</button>
+            ) : (
+              <button className="btn btn--green">Sign up</button>
+            )}
           </div>
         </form>
       </div>
